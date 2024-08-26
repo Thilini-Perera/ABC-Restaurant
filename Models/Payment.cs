142e8cc90933c90d2734ebd73ad5551be67ebdab
Thilini Perera <thiliniruwanthi.perera@gmail.com>
2024-08-26 19:14:08 +0530

    committing payment.cs

---
+++ Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABCResturant.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public float Total { get; set; }
+        public User? User { get; set; }
+        public int UserId { get; set; }
+        public Restuarant? Restuarant { get; set; }
+        public int RestuarantId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
