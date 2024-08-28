5eb464d9579f77413f1f6f3b3f6cd46a0292d034
Thilini Perera <thiliniruwanthi.perera@gmail.com>
2024-08-28 21:09:28 +0530

    committing user activity, users, weather forecast controllers

---
+++ Controllers/UserActivityController.cs
@@ -0,0 +1,224 @@
+﻿using ABCResturant.Models;
+using ABCResturant.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using static ABCResturant.Server.Controllers.ResturantController;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ABCResturant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserActivityController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private readonly IConfiguration _config;
+
+        public UserActivityController(ApplicationDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+
+        // GET: api/<UserActivityController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserActivityController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserActivityController>
+        [HttpPost]
+        public async Task Post(UserActivity value)
+        {
+
+            value.CreatedOn = DateTime.Now.ToLocalTime();
+            await _context.AddAsync(value);
+            await _context.SaveChangesAsync();
+        }
+
+        // PUT api/<UserActivityController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserActivityController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [Route("GetReport")]
+        [HttpGet]
+        public async Task<IActionResult> GetReport(int id)
+        {
+            List<UserActivity> dBEntities = await _context.UserActivities.Where(x => x.UserId == id).Include(x => x.User).ToListAsync();
+            if (dBEntities != null)
+            {
+                var pdfReportInfo = GenerateReport(dBEntities,true);
+                return File(pdfReportInfo.ByteArray, pdfReportInfo.MimeType, pdfReportInfo.FileName);
+            }
+            return Ok(new
+            {
+                StatusCode = 400,
+                Message = "Not found",
+                Data = (object)null
+            });
+        }
+        [Route("GetExtReport")]
+        [HttpGet]
+        public async Task<IActionResult> GetExtReport()
+        {
+            List<UserActivity> dBEntities = await _context.UserActivities.Include(x => x.User).ToListAsync();
+            if (dBEntities != null)
+            {
+                var pdfReportInfo = GenerateReport(dBEntities);
+                return File(pdfReportInfo.ByteArray, pdfReportInfo.MimeType, pdfReportInfo.FileName);
+            }
+            return Ok(new
+            {
+                StatusCode = 400,
+                Message = "Not found",
+                Data = (object)null
+            });
+        }
+
+        private static PdfReportFileInfo GenerateReport(List<UserActivity> dBEntities, bool isSingle = false)
+        {
+            User? user = dBEntities.OrderBy(x => x.Id).FirstOrDefault().User;
+            QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
+            Document document = Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    if (isSingle)
+                    {
+                        page.Header().Text($"User Activity Report from the {user.Name}").SemiBold().FontSize(20).FontColor(Colors.Blue.Medium);
+                    }
+                    else
+                    {
+                        page.Header().Text($"User Activity Report from the ABC Restuarant Chain").SemiBold().FontSize(20).FontColor(Colors.Blue.Medium);
+                    }
+
+                    page.Content()
+                    .PaddingVertical(5, Unit.Centimetre)
+                    .Column(column =>
+                    {
+                        // Add a horizontal line right below the header
+                        column.Item().Height(5).Background(Colors.Grey.Medium); // Adjust height, color, and margin as needed
+
+                        // Add a row for additional content or spacing
+                        column.Item().Row(row =>
+                        {
+                            row.AutoItem().PaddingHorizontal(10).LineVertical(1).LineColor(Colors.Grey.Medium);
+                        });
+
+                        // Main content area
+                        column.Item().Text("This report is generated.");
+
+                        // Create a table for the profit report
+                        column.Item().Table(table =>
+                        {
+                            table.ColumnsDefinition(columns =>
+                            {
+                                columns.ConstantColumn(6, Unit.Centimetre); // Width for dates
+                                if (isSingle)
+                                {
+                                    columns.RelativeColumn(); // Width for numbers
+                                }
+                                columns.RelativeColumn(); // Width for numbers
+                            });
+
+
+                            // Add header row
+                            table.Header(header =>
+                            {
+                                header.Cell().RowSpan(1).Text("Date").Bold();
+                                if (isSingle)
+                                {
+                                    header.Cell().RowSpan(1).Text("User").Bold();
+                                }
+                                header.Cell().RowSpan(1).Text("Activity").Bold();
+
+                            });
+
+                            // Add data rows
+                            // Example data; replace with actual data
+
+                            foreach (var entity in dBEntities)
+                            {
+                                if (entity != null)
+                                {
+                                    table.Cell().Text(entity.CreatedOn.ToShortDateString());
+                                    if (isSingle)
+                                    {
+                                        table.Cell().Text(entity.User?.Name);
+                                    }
+                                    table.Cell().Text(entity.Activity);
+                                }
+                               
+                            }
+
+                        });
+
+                    });
+
+                    page.Footer()
+                    .AlignCenter()
+                    .Text(x =>
+                    {
+                        x.Span("Page ");
+                        x.CurrentPageNumber();
+                    });
+
+                });
+            });
+            byte[] pdfBytes = document.GeneratePdf();
+
+            if (isSingle)
+            {
+                return new PdfReportFileInfo()
+                {
+                    ByteArray = pdfBytes,
+                    FileName = $"User Activity Report on {user.Name}.pdf",
+                    MimeType = "application/pdf"
+                };
+            
+            }
+
+            return new PdfReportFileInfo()
+            {
+                ByteArray = pdfBytes,
+                FileName = $"User Activity on ABC Restuarant.pdf",
+                MimeType = "application/pdf"
+            };
+        }
+
+        public class UserActivityModel
+        {
+            public int UserId { get; set;}
+            public string Activity { get; set;}
+            public DateTime CreatedOn { get; set;}
+        }
+    }
+
+
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,213 @@
+﻿using ABCResturant.Models;
+using ABCResturant.Persistance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABCResturant.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<List<User>> Get()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var dBEntity = await _context.Users.Where(x => x.Id == id).Select(x => new
+            {
+                x.Password,
+                x.Email,
+                CreatedOn = x.CreatedOn.ToLongDateString(),
+                UpdatedOn = x.UpdatedOn.Value.ToLongDateString(),
+                x.Name,
+                x.Telephone,
+                x.Role,
+            }).FirstOrDefaultAsync();
+            if (dBEntity != null)
+            {
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "Found",
+                    Data = dBEntity
+                });
+            }
+            return Ok(new
+            {
+                StatusCode = 400,
+                Message = "Not found",
+                Data = (object)null
+            });
+        }
+
+        [Route("GetByRole")]
+        [HttpGet]
+        public async Task<List<User>> GetByRole(string role)
+        {
+            return await _context.Users.Where(x => x.Role == role).ToListAsync();
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public async Task Post([FromBody] User value)
+        {
+            value.CreatedOn = DateTime.Now;
+            await _context.Users.AddAsync(value);
+            await _context.SaveChangesAsync();
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut]
+        public async Task Put([FromBody] User value)
+        {
+            User dBEntity = await _context.Users.FindAsync(value.Id);
+            if (dBEntity != null)
+            {
+                dBEntity.Name = value.Name;
+                dBEntity.Password = value.Password;
+                dBEntity.Email = value.Email;
+                dBEntity.Role = value.Role;
+                dBEntity.Telephone = value.Telephone;
+                dBEntity.UpdatedOn = DateTime.Now;
+
+                _context.Users.Update(dBEntity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            User dBEntity = await _context.Users.FindAsync(id);
+            if (dBEntity != null)
+            {
+                _context.Users.Remove(dBEntity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == model.Email && x.Password == model.Password);
+            if (user != null)
+            {
+                User defaultUser = new();
+                switch (user.Role)
+                {
+                    case "Admin":
+                        Admin admin = new Admin
+                        {
+                            Name = user.Name,
+                            Email = user.Email,
+                            Id = user.Id,
+                        };
+
+                        return Ok(new
+                        {
+                            StatusCode = 200,
+                            Message = "User Found",
+                            Data = admin
+                        });
+
+                    case "Staff":
+                        Staff staff = new Staff
+                        {
+                            Name = user.Name,
+                            Email = user.Email,
+                            Id = user.Id,
+                            Role = user.Role,
+                        };
+
+                        return Ok(new
+                        {
+                            StatusCode = 200,
+                            Message = "User Found",
+                            Data = staff
+                        });
+
+                    case "Customer":
+                        Customer customer = new Customer
+                        {
+                            Name = user.Name,
+                            Email = user.Email,
+                            Id = user.Id,
+                            Role = user.Role,
+                        };
+
+                        return Ok(new
+                        {
+                            StatusCode = 200,
+                            Message = "User Found",
+                            Data = customer
+                        });
+
+                    default:
+                        break;
+                }
+
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "User Found",
+                    Data = defaultUser
+                });
+            }
+            return BadRequest(new
+            {
+                StatusCode = 400,
+                Message = "Not found",
+                Data = (object)null
+            });
+
+        }
+
+        [Route("Logout")]
+        [HttpGet]
+        public async Task<IActionResult> Logout([FromBody] string email, int id)
+        {
+            return Ok(new
+            {
+                StatusCode = 201,
+                Message = "Log out Compelete",
+                Data = (object)null
+            });
+
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task Register([FromBody] User value)
+        {
+            value.CreatedOn = DateTime.Now;
+            value.Role = "Customer";
+            await _context.Users.AddAsync(value);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
